ef83032e6671375c49d8f1918c73747b42faf153
Guilherme Dalenogari de Freitas <66420028+guidalenogari@users.noreply.github.com>
2020-06-11 14:26:18 -0300

    #1
    First Version of the Commit of the Project

---
+++ java-basico/sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,13 @@
+
+public class TestaPontoFlutuante {
+
+	public static void main ( String [] args) {
+		
+		double salario;
+		salario = 1250.70;
+		
+		System.out.println("Meu Salário é " + salario);
+		
+	}
+	
+}
---
+++ java-basico/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,22 @@
+
+public class TestaVariaveis {
+
+	public static void main ( String [] args) {
+		System.out.println("Ola Novo Teste");
+		
+		int idade;
+		idade = 37;
+		
+		System.out.println(idade);
+		
+		idade = 30 + 10;
+		
+		System.out.println(idade);
+		
+		idade = (7 * 5) + 2; 
+		
+		System.out.println(idade);
+		
+	}
+	
+}
